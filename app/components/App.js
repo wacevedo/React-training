9bd91f68be81633c99b6193284c701eb918604c1
Willson Acevedo <wacevedo@intellisys.com.do>
2017-05-10 11:29:56 -0400

    Adding Dynamic Rendering for Battle and Query Params for Results

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ var React = require('react');
 var Popular = require('./Popular');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
@@ -17,6 +18,7 @@ class App extends React.Component {
                     <Switch>
                         <Route exact path='/' component={Home} />
                         <Route exact path='/battle' component={Battle} />
+                        <Route path='/battle/results' component={Results} />
                         <Route path='/popular' component={Popular} />
                         <Route render={function () {
                             return <p>Not Found</p>
