fc1724dfa7915752f15abda968205e66778804e1
Willson Acevedo <wacevedo@intellisys.com.do>
2017-05-24 09:10:46 -0400

    Adding babel polyfill

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -86,7 +86,7 @@ class Battle extends React.Component {
         this.setState(function (){
             let newState = {};
             newState[id + 'Name'] = username;
-            newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+            newState[id + 'Image'] = `https://github.com/${username}.png?size=200`;
             return newState;
         });
     }
